0005e133922b35d6dbdef8a6dfebbfbbb0d58b69
zak-dev-art <zachndungu861@gmail.com>
2025-09-25 13:09:30 +0300

    modified js file

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const text = document.getElementById("text");
+  text.textContent = "This is really cool!";
+});
